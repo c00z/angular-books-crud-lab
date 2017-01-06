5f906fb19a9a2df3d468653227de073ed598d342
c00z <zcusimano@gmail.com>
2017-01-06 15:18:07 -0800

    saving

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -2,9 +2,21 @@ angular.module('libraryApp')
   .controller('BooksShowController', BooksShowController);
 
 // controller function and dependency injection
-// $routeParams and $location are required for routing stuff 
+// $routeParams and $location are required for routing stuff
 //   - but you might need to add a dependency
-BooksShowController.$inject=['$routeParams', '$location'];   
+BooksShowController.$inject=['$routeParams', '$location'];
 function BooksShowController($routeParams, $location) {
   var vm = this;
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(function successCallback(response) {
+    console.log("Hitting Get SHOW", response);
+    vm.books = response.data.books;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+
 };
